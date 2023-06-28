6f069e4c97751e6616b640103220e9a42d0bd5e6
Jeremy-Bojko <jeremy.bojko@protonmail.com>
2023-06-28 17:56:44 +0200

    try again

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,12 @@
-import { Component } from '@angular/core';
+import { DOCUMENT } from "@angular/common";
+import { Component, Inject, OnInit, Renderer2 } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
-export class AppComponent {
-}
-
+export class AppComponent {}
 
 /*
 Copyright Google LLC. All Rights Reserved.
---
+++ src/environments/environment.ts
@@ -0,0 +1,3 @@
+export const environment = {
+  baseHref: "/get-started-doc-angular",
+};
